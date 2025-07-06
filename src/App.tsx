b82a82a29d2daa41dd3460d6c8c674a44d118fe8
devRohitGangurde <rohitgangurde@Rohits-MacBook-Air.local>
2025-07-06 15:01:43 +0530

    typescript setup added

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+
+import Body from './components/Body';
+import React from 'react';
+
+const App: React.FC = () => {
+  return (
+    <>
+    <Body/>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Browse.tsx
@@ -0,0 +1,7 @@
+const Browse: React.FC = () => {
+  return (
+    <div>Browse</div>
+  )
+}
+
+export default Browse;
